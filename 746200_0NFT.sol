41d0ad916a30f4f97b2ef0e7fb09dbef1c453c01
HLABZ® <113842155+HLABZcc@users.noreply.github.com>
2022-12-10 02:50:04 +0100

    Update 746200_0NFT.sol

--- 746200_0NFT.sol
+++ 746200_0NFT.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: MIT
 
+// ##     ## ##          ###    ########  ########  ######   ######  
+// ##     ## ##         ## ##   ##     ##      ##  ##    ## ##    ## 
+// ##     ## ##        ##   ##  ##     ##     ##   ##       ##       
+// ######### ##       ##     ## ########     ##    ##       ##       
+// ##     ## ##       ######### ##     ##   ##     ##       ##       
+// ##     ## ##       ##     ## ##     ##  ##      ##    ## ##    ## 
+// ##     ## ######## ##     ## ########  ########  ######   ######  
+
 pragma solidity ^0.8.0;
 
 import "./IONFT721.sol";
 import "./ONFT721Core.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// NOTE: this ONFT contract has no public minting logic.
-// must implement your own minting logic in child classes
 contract ONFT721 is ONFT721Core, ERC721, IONFT721 {
     constructor(string memory _name, string memory _symbol, address _lzEndpoint) ERC721(_name, _symbol) ONFT721Core(_lzEndpoint) {}
 
